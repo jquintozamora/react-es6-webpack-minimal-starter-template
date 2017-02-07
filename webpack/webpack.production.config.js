cb1a1cd4a506c4c5c740176dadb1ea90bf015a0a
Jose Quinto <quintorel@hotmail.com>
2017-02-07 21:49:35 +0000

    webpack change

--- webpack/webpack.production.config.js
+++ webpack/webpack.production.config.js
@@ -100,8 +100,7 @@ module.exports = {
             {
               loader: 'sass-loader',
               options: {
-                sourceMap: true,
-
+                sourceMap: true
               },
             }
           ]
