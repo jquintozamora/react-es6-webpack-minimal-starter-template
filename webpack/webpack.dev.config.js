5c7061d609af2eac8cd9f6c41f2a78ce9de63d8d
Jose Quinto <quintorel@hotmail.com>
2017-02-23 22:23:04 +0000

    pluging deprecated

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -75,14 +75,7 @@ module.exports = {
      */
     new webpack.HotModuleReplacementPlugin(),
     // prints more readable module names in the browser console on HMR updates
-    new webpack.NamedModulesPlugin(),
-    /**
-      * When there are errors while compiling this plugin skips the emitting phase (and recording phase), 
-      * so there are no assets emitted that include errors. The emitted flag in the stats is false for all assets. 
-      * If you are using the CLI, the webpack process will not exit with an error code by enabling this plugin. 
-      * If you want webpack to "fail" when using the CLI, please check out the bail option
-      */
-    new webpack.NoErrorsPlugin()
+    new webpack.NamedModulesPlugin()
   ],
   module: {
     // loaders -> rules in webpack 2
